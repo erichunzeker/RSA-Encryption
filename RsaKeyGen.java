47513432ae257fa8cf8074c28b1510154e8585a9
Eric Hunzeker <emh128.pitt.edu>
2018-04-06 14:46:31 -0400

    finish RsaKeyGen, started RsaSign, and update gitignore

--- RsaKeyGen.java
+++ RsaKeyGen.java
@@ -1,3 +1,32 @@
-public class RsaKeyGen {
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
 
+public class RsaKeyGen {
+    public static void main(String[] args) {
+        Random random = new Random();
+        LargeInteger p, q, n, phiN, e, d;
+        LargeInteger one = new LargeInteger(new byte[(byte) 1]);
+        
+        p = new LargeInteger(256, random);
+        q = new LargeInteger(256, random);
+        n = p.multiply(q);
+        phiN = (p.subtract(one)).multiply(q.subtract(one));
+        e = new LargeInteger(new byte[(byte) 65537]);
+        d = e.modularExp(one.negate(), phiN);
+
+        try {
+            FileOutputStream pubkey = new FileOutputStream("pubkey.rsa");
+            pubkey.write(e.getVal());
+            pubkey.write(n.getVal());
+            pubkey.close();
+            FileOutputStream privkey = new FileOutputStream("privkey.rsa");
+            privkey.write(d.getVal());
+            privkey.write(n.getVal());
+            privkey.close();
+
+        } catch (IOException i) {
+            System.out.println("File error");
+        }
+    }
 }
--- RsaSign.java
+++ RsaSign.java
@@ -1,2 +1,30 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class RsaSign {
+    public static void main(String[] args) {
+        try {
+            Path path = Paths.get(args[1]);
+            byte[] input = Files.readAllBytes(path);
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(input);
+            byte[] digest = md.digest();
+
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("Algorithm not found");
+        } catch (IOException e) {
+            System.out.println("Error reading input file");
+        }
+
+        if (args[0].equalsIgnoreCase("s")) {
+
+        }
+        else if (args[0].equalsIgnoreCase("v")) {
+
+        }
+    }
 }
